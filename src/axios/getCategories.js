0ab454a205d03fb7a90eb8beb4675aa8d6f74cfd
Webdatadevs <webpydevs@gmail.com>
2024-03-23 12:43:49 +0500

    url

--- src/axios/getCategories.js
+++ src/axios/getCategories.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { setCategories } from "../slice/Categories.Slice"
 
 export default async function getCategories(dispatch){
-    await axios.get("https://datab-quej.onrender.com/categories")
+    await axios.get("https://datab-3.onrender.com/categories")
     .then((data) => {
         dispatch(setCategories(data.data))
         console.log(data.data)
--- src/axios/getProducts.js
+++ src/axios/getProducts.js
@@ -5,7 +5,7 @@ import { setCategories } from "../slice/Categories.Slice";
 export default async function getProducts(dispatch) {
     dispatch(isLoadingProduct());
     await axios
-        .get("https://datab-quej.onrender.com/products")
+        .get("https://datab-3.onrender.com/products")
         .then((data) => {
             dispatch(setProducts(data.data));
             console.log(data.data);
