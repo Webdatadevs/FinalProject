3c574d01b4951752c1c790142606a0a304d7ab5d
Webdatadevs <webpydevs@gmail.com>
2024-03-27 10:10:08 +0500

    add price

--- src/slice/Product.Slice.js
+++ src/slice/Product.Slice.js
@@ -58,7 +58,7 @@ export const productSlice = createSlice({
                 item.id === action.payload ? { ...item, quantyti: item.quantyti + 1 } : item
             );
             const data = state.basket.find(element => +element.id === +action.payload)
-            state.totalPrice += Math.round(sliceData)
+            state.totalPrice += Math.round(data.price)
         },
         decrementQuantity(state, action) {
             state.basket = state.basket.map(item =>
@@ -68,7 +68,7 @@ export const productSlice = createSlice({
             if(state.totalPrice===0){
                 return;
             }else{
-                state.totalPrice -= Math.round(sliceData)
+                state.totalPrice -= data.price
             }
         },
         setSelectProducts(state, action){
