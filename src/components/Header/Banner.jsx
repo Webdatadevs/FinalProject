60448965f64ae007a258de85ccab00ed92690ff5
Webdatadevs <webpydevs@gmail.com>
2024-06-20 14:37:06 +0500

    delete iframe

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -72,7 +72,7 @@ const Banner = () => {
                         <Img width={"100%"} height={"100%"} src="banner.png" />
                     </Box>
                 </Box>
-                <div id='frame' style='width:728px;'><iframe data-aa='2333549' src='//ad.a-ads.com/2333549?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style='display: block; text-align: right; font-size: 12px' id='preview-link' href='https://a-ads.com/?partner=2333549'>Advertise here</a></div>
+                {/* <div id='frame' style='width:728px;'><iframe data-aa='2333549' src='//ad.a-ads.com/2333549?size=728x90' style='width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe><a style='display: block; text-align: right; font-size: 12px' id='preview-link' href='https://a-ads.com/?partner=2333549'>Advertise here</a></div> */}
             </Box>
         </>
     );
